49420f3233ac16e0ef0c49b4b705aa67630d4cb8
RekhuGopal <vrchinnarathod@gmail.com>
2024-06-09 15:08:50 +0530

    Updated changes

--- provider.tf
+++ provider.tf
@@ -6,9 +6,9 @@ provider "aws" {
 # configure backend
 terraform {
   backend "s3" {
-    bucket         = "dhsoni-terraform"
-    key            = "aws-eks.terraform.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "terraform-state-lock-dynamodb"
+    bucket         = "terraformstates3bucket"
+    key            = "aws-eks-terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "terraform_state"
   }
 }
